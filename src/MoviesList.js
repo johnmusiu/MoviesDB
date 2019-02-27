11fae89bd30b379bb25d5a81d1e40e74ab4a146a
John Musiu <johnmusiu@gmail.com>
2019-02-27 19:22:52 +0300

    Add routing and links

---
+++ src/MoviesList.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Movie from "./Movie";
+
+class MoviesList extends Component {
+  state = {
+    movies: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        "https://api.themoviedb.org/3/discover/movie?api_key=8c428d12cd39a89cbf5c7d1347ece57d&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+      );
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.movies.map(movie => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default MoviesList;
