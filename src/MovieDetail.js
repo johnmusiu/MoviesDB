f09a26b669536d25b62d1ec534dfa6c215e459e0
John Musiu <johnmusiu@gmail.com>
2019-02-28 06:27:45 +0300

    Add movie details

---
+++ src/MovieDetail.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+const POSTER_PATH = "http://image.tmdb.org/t/p/w154";
+const BACKDROP_PATH = "http://image.tmdb.org/t/p/w1280";
+
+class MovieDetail extends Component {
+  state = {
+    movie: {}
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/movie/${
+          this.props.match.params.id
+        }?api_key=8c428d12cd39a89cbf5c7d1347ece57d`
+      );
+      const movie = await res.json();
+      this.setState({
+        movie
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    const { movie } = this.state;
+    return (
+      <div>
+        <img src={`${BACKDROP_PATH}${movie.backdrop_path}`} alt={movie.title} />
+        <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+        <h1>{movie.title}</h1>
+        <p>{movie.release_date}</p>
+        <p>{movie.overview}</p>
+      </div>
+    );
+  }
+}
+
+export default MovieDetail;
